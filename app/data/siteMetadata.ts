25c5b6577c62d529bcef19de2e6b7c25d60fab5f
Omar <omarkshaikh@gmail.com>
2025-07-20 10:50:54 +0400

    changed site metadata

--- app/data/siteMetadata.ts
+++ app/data/siteMetadata.ts
@@ -6,24 +6,24 @@ export const siteMetadata = {
     "I'm Omar, a software engineer, blogger and international speaker. Welcome to my corner of the internet!",
   language: "en-us",
   siteUrl: "https://omarshayk.com",
-  siteRepo: "https://github.com/braydoncoyer/braydoncoyer.dev",
+  siteRepo: "https://github.com/OmarShaikh/portfolio25",
   avatarImage:
-    "https://res.cloudinary.com/braydoncoyer/image/upload/v1636553710/avatar.jpg",
+    "https://drive.omars.cloud/apps/files_sharing/publicpreview/reZ7Xfe8KYFbwi6?file=/&fileId=1232&x=1152&y=2048&a=true&etag=d8e465801575b7c77fd71a6514609474",
   socialBanner:
-    "https://res.cloudinary.com/braydoncoyer/image/upload/v1638884997/default-social-image.png",
+    "https://drive.omars.cloud/apps/files_sharing/publicpreview/9fK6xpjaci7ymyw?file=/&fileId=1242&x=1152&y=2048&a=true&etag=c35293eb7d3687f8abde2b9b4f186946",
   email: "mailto:omarshaykhh@gmail.com",
   github: "https://github.com/OmarShaikh",
   twitter: "https://x.com/omarshaykx",
   twitterHandle: "@omarshaykx",
-  codepen: "https://codepen.io/braydoncoyer",
+  codepen: "https://codepen.io/omarshaykh",
   linkedin: "https://www.linkedin.com/in/omarsha1kh/",
-  resume: "/static/braydoncoyer_resume.pdf",
+  resume: "/static/omarshaykh_resume.pdf",
   snippets:
     "https://www.notion.so/9f35162787e0448094188b66304cf9f6?v=1b9c4e146edf46148fe7b1c5346cbea4",
   carbonCode: "CESDE27N",
   carbonPlacement: "braydoncoyerdev",
   locale: "en-US",
   analytics: {
-    plausibleDataDomain: "braydoncoyer.dev",
+    plausibleDataDomain: "omarshayk.com",
   },
 };
